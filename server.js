c6255682f456789e5bcc6a889c54d45230bc0361
beberiche <woaol@naver.com>
2021-09-07 23:44:11 +0900

    Updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const mongoose = require(`mongoose`);
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('unhandledRejection', (err) => {
-  console.log('UNHANDLER REJECTION! 🧨 🧨 🧨 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
-});
-
 process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION! 🧨 🧨 🧨 Shutting down...');
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
@@ -26,12 +20,18 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
-  .then(() => console.log('DB connection successful'));
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
