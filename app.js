5f2466b4aff609804a411e26108f64cd37bbd0d3
beberiche <woaol@naver.com>
2021-09-08 14:36:23 +0900

    Improved stripe implementation

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./routes/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 // Start express app
@@ -118,6 +119,12 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
